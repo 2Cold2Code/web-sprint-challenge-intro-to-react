48ac929fd224dda360bedf88c5e51279c89d3b6b
Adam Osburn <wizardofosburn@gmail.com>
2020-06-28 21:41:14 -0400

    created Character component. having issues with the props for unknown reasons.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
+import React from 'react';
 // Write your Character component here
+export default function Character(
+    {name}, 
+    {gender},
+    {birthYear},
+    {height},
+    {mass},
+    {hairColor},
+    {species}
+    ){
+
+
+        return (
+            <div>
+                <h2 className='name'>{name}</h2>
+                <p className='gender'>{gender}</p>
+                <p className='birth-year'>{birthYear}</p>
+                <p className='height'>{height}</p>
+                <p className='mass'>{mass}</p>
+                <p className='hair-color'>{hairColor}</p>
+                <p className='species'>{species}</p>
+            </div>
+        )
+}
