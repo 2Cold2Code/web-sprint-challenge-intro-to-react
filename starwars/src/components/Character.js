557418caa9c8d8c70ec9c8541df1963427efd947
Adam Osburn <wizardofosburn@gmail.com>
2020-07-05 20:45:06 -0400

    imported components for sections and h2, added component to... return statement. Passed style props to components. Added labels to the HTML text in addition to the data

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,17 +1,37 @@
-import React from 'react';
-// Write your Character component here
-export default function Character(props){
-  const {name, gender, birthYear, height, mass, hairColor, species} = props;
+import React from "react";
+import {Sections} from '../styles/section_style';
+import {H2} from '../styles/h2_style'
 
-        return (
-            <div>
-                <h2 className='name'>{name}</h2>
-                <p className='gender'>{gender}</p>
-                <p className='birth-year'>{birthYear}</p>
-                <p className='height'>{height}</p>
-                <p className='mass'>{mass}</p>
-                <p className='hair-color'>{hairColor}</p>
-                <p className='species'>{species}</p>
-            </div>
-        )
+// Write your Character component here
+export default function Character(props) {
+  const { name, gender, birthYear, height, mass, hairColor, species } = props;
+  
+  return (
+    <Sections
+      hght={"hght"}
+      bGColor={"bGColor"}
+      direction={"direction"}
+      jC={"jC"}
+      aI={"aI"}
+    >
+      <H2
+        wth={"wth"}
+        hght={"hght"}
+        bgColor={"bgColor"}
+        direction={"direction"}
+        jC={"jC"}
+        aI={"aI"}
+        color={'color'}
+        className="name"
+      >
+        Name: {name}
+      </H2>
+      <p className="gender">Gender: {gender}</p>
+      <p className="birth-year">Birth Year: {birthYear}</p>
+      <p className="height">Height: {height}</p>
+      <p className="mass">Mass: {mass}</p>
+      <p className="hair-color">Hair Color: {hairColor}</p>
+      <p className="species">Species: {species}</p>
+    </Sections>
+  );
 }
